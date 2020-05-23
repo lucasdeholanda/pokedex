73e7217caf17ff6bf7b7352b065762a22aef0fd5
Lucas de Holanda <lucas.holanda.vr@gmail.com>
2020-05-23 16:18:04 -0300

    Updates

---
+++ precache-manifest.028c77082882d79e424040f014e57670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3019bd1f3949e3111c3c92d807edcec",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0d487757a2bd17874e17",
+    "url": "/pokedex/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a928562db6fa226a8c35",
+    "url": "/pokedex/static/js/2.ad2517f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.ad2517f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d487757a2bd17874e17",
+    "url": "/pokedex/static/js/main.41e81d04.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pokedex/static/media/logo.ee7cd8ed.svg"
+  }
+]);
